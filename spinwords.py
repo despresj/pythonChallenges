51a08b78fa8824e99c9034854bf56526875b501c
despresj <despresj@mail.gvsu.edu>
2021-03-02 09:21:57 -0500

    adding spinwords.py

---
+++ spinwords.py
@@ -0,0 +1,29 @@
+
+"""
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+
+spinWords( "This is a test") => returns "This is a test"
+
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+sentence = "This is another test"
+
+"""
+
+def spin_words(sentence):
+    sentence = sentence.split(" ")
+
+    newlist = []
+    for word in sentence:
+        if len(word) > 4:
+            newlist.append(word[::-1])
+            print()
+        else:
+            newlist.append(word)
+    return ' '.join(newlist)
+
+sentence = "This is another test"
+spin_words(sentence)
