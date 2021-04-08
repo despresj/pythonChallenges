13090839543250b65e02bffdca2b17cfcc1e5829
despresj <despresj@mail.gvsu.edu>
2021-04-08 09:01:17 -0400

    working on cleaning

--- data_cleaning_challenge/upc/cleaning.py
+++ data_cleaning_challenge/upc/cleaning.py
@@ -1,36 +1,7 @@
 import pandas as pd
 import re
 
-pd.set_option("display.max_rows", 101)
-xl_file = pd.ExcelFile("data/MSF_Catagories.xlsx", engine='openpyxl')
-
-dfs = {sheet_name: xl_file.parse(sheet_name) 
-          for sheet_name in xl_file.sheet_names}
-
-df = dfs['Report1']
-
-def the_col_cleaner(df):
-    cols = df.columns
-    df.columns = [re.sub(r'\W+', '', x.replace('$','dollars')).lower() for x in cols]
-    return(df)
-
-the_col_cleaner(df)
-
-df['allperiods'] = df['allperiods'].str.replace('Latest 52 Wks - W/E', '')
-
-for column in df.columns:
-    print(df[column])
-    
-xl_file1 = pd.ExcelFile("data/MSF_UPC.xlsx", engine='openpyxl')
-dfs = {sheet_name: xl_file1.parse(sheet_name) 
-          for sheet_name in xl_file1.sheet_names}
-
-dfs.keys()
-
-dfs['Report1']
-
-cleaned = the_col_cleaner(dfs['Report1'])
-cleaned.columns
+pd.set_option("display.max_rows", 30)
 
 def exceldict(path):
     xl_file1 = pd.ExcelFile(path, engine='openpyxl')
@@ -38,10 +9,79 @@ def exceldict(path):
             for sheet_name in xl_file1.sheet_names}
     return dfs
 
-dfs = exceldict("data/KUSA Price List - Frozen Veggie.xlsx")
+catagories = exceldict("data/MSF_Catagories.xlsx")
+catagories
+for key in catagories.keys():
+    print(key)
+
+def n_sheets(dict):
+    output = str(len(dict.keys())) + " sheets"
+    print(output)
+    print("------")
+    for key in dict.keys():
+        print(key)
+
+n_sheets(catagories)
+
+a, b, c = categories.values()
+a # not helpful
+b # not helpful
+c
+
+report = catagories['Report1']
+
+def the_col_cleaner(df):
+    cols = df.columns
+    df.columns = [re.sub(r'\W+', '', x.replace('$','dollars')).lower() for x in cols]
+    return(df)
+
+msf = the_col_cleaner(report)
+
+msf['allperiods'] = msf['allperiods'].str.replace('Latest 52 Wks - W/E', '')
+msf['allproducts'] = msf['allproducts'].str.replace('MORNINGSTAR FARMS-', '')
+
+msf[['product', 'category', 'upc1']] = msf['allproducts'].str.split('-', 2, expand=True)
+
+upc = msf['upc'].astype(float)
+upc1= msf['upc1'].astype(float)
+
+upc[upc != upc1]
+# same info drop?
+msf_clean = msf[['dollars', 'units', 'product', 'category', 'upc']]
+msf_clean
+
+# UPC
+
+upc_dict = exceldict("data/MSF_UPC.xlsx")
+
+n_sheets(upc_dict)
+
+# sepaerate sheets
+a, b, c = upc_dict.values()
+a # not helpful
+b # not helpful
+c
+
+upc = the_col_cleaner(upc_dict['Report1'])
+upc
+
+for df in upc_dict:
+    print(upc_dict[df])
+    print("|||||||||||||||||||||||||||||||||||")
+
+upc_cleaned = the_col_cleaner(upc_dict['Report1'])
+upc_cleaned.columns
+
+
+
+veggies = exceldict("data/KUSA Price List - Frozen Veggie.xlsx")
+
+veggies
 
 dfs['Frozen Foods'].columns = dfs['Frozen Foods'].iloc[2]
 
 dfs['Frozen Foods'].columns 
 for key in dfs.keys():
     print(dfs[key])
+    
+    
