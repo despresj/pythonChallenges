3fb62ce8aa95bf138a03b6e87117fe3cf08f52e0
despresj <despresj@mail.gvsu.edu>
2021-04-13 08:33:53 -0400

    adding bootstrap and desc stats

--- pandas/application3.py
+++ pandas/application3.py
@@ -15,16 +15,35 @@ df_yes_no.groupby("harass5").count()
 pct = df_yes_no.groupby('harass5').agg({'harass5': 'count'})
 pct.apply(lambda x: 100*x/x.sum())
 
+[np.random.choice(df_yes_no.harass5, len(df), replace=True) for _ in range(1000)]
 
-reps = np.array([np.random.choice(df_yes_no.harass5, len(df), replace = True) for _ in range(1000)])
-Mean = np.count(reps)
+reps = np.array([np.random.choice(df_yes_no.harass5, len(df), replace=True) for _ in range(1000)])
+mean = pd.DataFrame(reps)
+mean
 
-# pick up here TODO: get this one col bootstrap pcts.
-rep = pd.DataFrame([np.random.choice(df_yes_no.harass5, len(df), replace=True) for _ in range(1000)])
+output = []
+sum_yes = []
+sum_no = []
+niters = 100000
+for _ in range(niters):
+  sample = np.random.choice(df_yes_no.harass5, len(df_yes_no), replace=True)
+  unique, counts = np.unique(sample, return_counts=True)
+  yes, no = counts
+  sum_yes.append(yes)
+  sum_no.append(no)
 
-rep.columns = ['answer']
+full = [sum_yes, sum_no]
+  
+bootstrapped = pd.DataFrame(list(zip(sum_yes, sum_no)), columns = ['yes', 'no'])
 
-counts = rep.groupby('answer').agg({'answer': 'count'})
-counts.apply(lambda x: 100*x/x.sum())
+total = len(df_yes_no)
+f = lambda x: x / total
 
+pcts = bootstrapped.apply(f)
+pcts.describe()
+
+
+
+df.groupby(['born']).size()
+df.groupby(['educ']).size()
 
